6d8388958d639a4178708f3cd66ed48bae6d17dd
IFAN RIFALDI <ifanrifaldi14@gmail.com>
2022-10-11 18:51:05 +0700

    Update iar.sql

--- public/iar.sql
+++ public/iar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 03, 2022 at 06:38 AM
+-- Generation Time: Oct 11, 2022 at 11:50 AM
 -- Server version: 5.7.33
 -- PHP Version: 8.1.9
 
@@ -24,12 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `divisis`
+--
+
+CREATE TABLE `divisis` (
+  `id` int(11) NOT NULL,
+  `nama_divisi` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `divisis`
+--
+
+INSERT INTO `divisis` (`id`, `nama_divisi`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'ifan', '123', '2022-10-04 05:54:21', '2022-10-04 05:54:21');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pembelians`
 --
 
 CREATE TABLE `pembelians` (
   `id` int(11) NOT NULL,
-  `nomor` int(25) DEFAULT NULL,
+  `nomor` varchar(255) DEFAULT NULL,
   `tanggal_pengajuan` varchar(255) DEFAULT NULL,
   `tanggal_dibutuhkan` varchar(255) DEFAULT NULL,
   `nama` varchar(255) DEFAULT NULL,
@@ -52,12 +73,41 @@ CREATE TABLE `pembelians` (
 --
 
 INSERT INTO `pembelians` (`id`, `nomor`, `tanggal_pengajuan`, `tanggal_dibutuhkan`, `nama`, `nama_divisi`, `nama_kegiatan`, `nama_barang`, `deskripsi_barang`, `foto`, `jumlah`, `lokasi`, `class`, `diketahui_oleh`, `disetujui_oleh`, `created_at`, `updated_at`) VALUES
-(7, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-10-02 23:20:59', '2022-10-02 23:20:59'),
-(9, 1234, NULL, 'we', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-10-02 23:28:05', '2022-10-02 23:28:05');
+(4, 'IAR2022101', NULL, '2022-10-15', 'IFAN RIFALDI', 'iarrrrr', 'yaaaaaa', 'laptop', 'blabla', NULL, 1, 'e3f', 'Pangan', 'ifan', 'rifaldi', '2022-10-07 18:43:11', '2022-10-09 02:39:44'),
+(5, 'IAR2022102102', '2022-10-08', '2022-10-08', NULL, 'iarrrrr', NULL, NULL, NULL, NULL, NULL, NULL, 'Pilih', NULL, NULL, '2022-10-07 18:47:57', '2022-10-07 18:47:57'),
+(6, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-10-10 22:21:22', '2022-10-10 22:21:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `password`, `created_at`, `updated_at`) VALUES
+(123, '123', '2022-10-10 21:53:43', '2022-10-10 21:53:43'),
+(1234, '1234', '2022-10-09 03:09:23', '2022-10-09 03:09:23'),
+(12345, '$2y$10$YKLRCE05jdyV6SSDdmR4RepDFu58Zj43uoj7Nml/ugUclU95qX26u', '2022-10-11 04:03:38', '2022-10-11 04:03:38');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `divisis`
+--
+ALTER TABLE `divisis`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `pembelians`
@@ -66,14 +116,32 @@ ALTER TABLE `pembelians`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `divisis`
+--
+ALTER TABLE `divisis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `pembelians`
 --
 ALTER TABLE `pembelians`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12346;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
