4c554d159605fb553dfd5c1ecf231e14eae286ef
IFAN RIFALDI <ifanrifaldi14@gmail.com>
2022-10-20 19:14:13 +0700

    delete

--- app/Models/Pembelian.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-use App\models\Traits\Attributes\PembelianAttributes;
-
-class Pembelian extends Model
-{
-    use PembelianAttributes;
-}
--- resources/views/template/utils/errors.blade.php
+++
@@ -1,5 +0,0 @@
-@if($errors->has($item))
-    @foreach($errors->get($item) as $msg)
-    <label for="" class="label text-danger">{{$msg}}</label>
-    @endforeach
-@endif
